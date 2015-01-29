6a593f104b967f34a9293e99559612cffb5e6de4
robertsalayjr <rws218@lehigh.edu>
2015-01-29 00:07:49 +0000

    finished WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Robert Salay
+//HW 01
+//Welcome Class Java Project
+//
+// First compile program 
+// javac WelcomeClass.java
+// run program
+// java WelcomeClass
+        
+//define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        //Print out welcome design
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-R--W--S--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Just before Christmas, my family got a new dog.");
+        System.out.println("Her name is Bella, and she chews on everything.");
+    }
+}
